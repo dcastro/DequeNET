95504938502e2206230f670addbdad73a5abf8bc
Diogo Castro <diogo.filipe.acastro@gmail.com>
2013-12-21 16:51:10 +0000

    Added IConcurrentDeque<T> interface

---
+++ DequeNet/IConcurrentDeque`1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DequeNet
+{
+    public interface IConcurrentDeque<T>
+    {
+        void PushRight(T item);
+        void PushLeft(T item);
+
+        bool TryPopRight(out T item);
+        bool TryPopLeft(out T item);
+    }
+}
