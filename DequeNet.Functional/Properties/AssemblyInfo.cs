c237e8da97270649e2db5f6c3020f53ca7c2b547
Diogo Castro <diogo.filipe.acastro@gmail.com>
2013-12-22 01:50:51 +0000

    Added functional tests project

--- DequeNet.Functional/Properties/AssemblyInfo.cs
+++ DequeNet.Functional/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DequeNet")]
+[assembly: AssemblyTitle("DequeNet.Functional")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DequeNet")]
+[assembly: AssemblyProduct("DequeNet.Functional")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("105b6e42-970c-4583-8491-14b1e127a913")]
+[assembly: Guid("05a210d0-78fb-404f-b011-71c6240a42b9")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,6 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: InternalsVisibleTo("DequeNet.Unit")]
-[assembly: InternalsVisibleTo("DequeNet.Functional")]
