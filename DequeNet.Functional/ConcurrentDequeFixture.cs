e2d53baece088b4a967f3fead8dc3d746f45a1a7
Diogo Castro <diogo.filipe.acastro@gmail.com>
2013-12-24 14:05:20 +0000

    Added functional test for interleaved push left/pop left operations

--- DequeNet.Functional/ConcurrentDequeFixture.cs
+++ DequeNet.Functional/ConcurrentDequeFixture.cs
@@ -56,5 +56,51 @@ namespace DequeNet.Functional
             long actualSum = deque.GetNodes().Sum(n => (long) n._value);
             Assert.Equal(sum, actualSum);
         }
+
+        /// <summary>
+        /// Verifies that parallel interleaved push right/pop left operations don't leave the deque in a corrupted state
+        /// </summary>
+        [Fact]
+        public void InterleavedPushPopLeftOps()
+        {
+            //Arrange
+            const int threadCount = 20;
+            const int runningTime = 3000;
+
+            var deque = new ConcurrentDeque<int>();
+            long sum = 0;
+            bool cancelled = false;
+
+            ThreadStart action = () =>
+            {
+                Random rnd = new Random();
+
+                while (!cancelled)
+                {
+                    //slightly biased towards "push"
+                    if (rnd.NextDouble() >= 0.45)
+                    {
+                        //push
+                        var val = rnd.Next(1, 51);
+                        deque.PushLeft(val);
+                        Interlocked.Add(ref sum, val);
+                    }
+                    else
+                    {
+                        //pop
+                        int val;
+                        if (deque.TryPopLeft(out val))
+                            Interlocked.Add(ref sum, -val);
+                    }
+                }
+            };
+
+            //Act
+            action.RunInParallel(() => cancelled = true, threadCount, runningTime);
+
+            //Assert
+            long actualSum = deque.GetNodes().Sum(n => (long)n._value);
+            Assert.Equal(sum, actualSum);
+        }
     }
 }
