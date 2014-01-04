ca20d2695ed1745199e87ffd262bad66911fd0da
Diogo Castro <diogo.filipe.acastro@gmail.com>
2014-01-04 18:11:45 +0000

    Added modular arithmetics

---
+++ DequeNet.Unit/Extensions/IntExtensionsTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DequeNet.Extensions;
+using Xunit;
+using Xunit.Extensions;
+
+namespace DequeNet.Unit.Extensions
+{
+    public class IntExtensionsTests
+    {
+        [Theory]
+        [PropertyData("GetTestData")]
+        public void Mod_ReturnsCorrectModulo(int dividend, int divisor, int expectedMod)
+        {
+            Assert.Equal(expectedMod, dividend.Mod(divisor));
+        }
+
+        [Fact]
+        public void Mod_ThrowsException_IfDivisorIsZero()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => 1.Mod(0));
+        }
+
+        public static IEnumerable<object[]> GetTestData
+        {
+            get
+            {
+                yield return new object[] {1, 1, 0};
+                yield return new object[] {0, 1, 0};
+                yield return new object[] {2, 10, 2};
+                yield return new object[] {12, 10, 2};
+                yield return new object[] {22, 10, 2};
+                yield return new object[] {-2, 10, 8};
+                yield return new object[] {-12, 10, 8};
+                yield return new object[] {-22, 10, 8};
+                yield return new object[] { 2, -10, -8 };
+                yield return new object[] { 12, -10, -8 };
+                yield return new object[] { 22, -10, -8 };
+                yield return new object[] { -2, -10, -2 };
+                yield return new object[] { -12, -10, -2 };
+                yield return new object[] { -22, -10, -2 };
+            }
+        }
+    }
+}
---
+++ DequeNet/Extensions/IntExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DequeNet.Extensions
+{
+    internal static class IntExtensions
+    {
+        /// <summary>
+        /// Applies the modulo operator.
+        /// </summary>
+        /// <param name="a">The dividend.</param>
+        /// <param name="n">The divisor. Must be different than zero.</param>
+        /// <returns>The result of (<paramref name="a"/> mod <paramref name="n"/>).</returns>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="n"/> is zero.</exception>
+        internal static int Mod(this int a, int n)
+        {
+            if (n == 0)
+                throw new ArgumentOutOfRangeException("n", "(a mod 0) is undefined.");
+
+            //puts a in the [-n+1, n-1] range using the remainder operator
+            int remainder = a%n;
+
+            //if the remainder is less than zero, add n to put it in the [0, n-1] range if n is positive
+            //if the remainder is greater than zero, add n to put it in the [n-1, 0] range if n is negative
+            if ((n > 0 && remainder < 0) ||
+                (n < 0 && remainder > 0))
+                return remainder + n;
+            return remainder;
+        }
+    }
+}
