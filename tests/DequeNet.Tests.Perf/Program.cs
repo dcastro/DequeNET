7633eb4bf46e462717a60099569c15d45b3c7c0a
Diogo Castro <diogo.filipe.acastro@gmail.com>
2014-03-27 09:43:07 +0000

    Created project for perf tests

---
+++ tests/DequeNet.Tests.Perf/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using DequeNet.Tests.Common;
+
+namespace DequeNet.Tests.Perf
+{
+    public static class Program
+    {
+        private const int RunningTime = 5000;
+        private const int ThreadCount = 4;
+
+        public static void Main(string[] args)
+        {
+            var deque = new ConcurrentDeque<int>(Enumerable.Repeat(1, 100000));
+            bool cancelled = false;
+
+            Action action = () =>
+                {
+                    var rnd = new Random(Thread.CurrentThread.ManagedThreadId);
+
+                    while (!cancelled)
+                    {
+                        PerformRandomAction(deque, rnd);
+                    }
+                };
+
+            action.RunInParallel(() => cancelled = true, ThreadCount, RunningTime);
+        }
+
+        private static void PerformRandomAction(IConcurrentDeque<int> deque, Random rnd)
+        {
+            int randomOp = rnd.Next(4);
+            int poppedValue;
+
+            switch (randomOp)
+            {
+                case 0:
+                    deque.PushLeft(1);
+                    break;
+                case 1:
+                    deque.PushRight(1);
+                    break;
+                case 2:
+                    deque.TryPopLeft(out poppedValue);
+                    break;
+                case 3:
+                    deque.TryPopRight(out poppedValue);
+                    break;
+            }
+        }
+    }
+}
